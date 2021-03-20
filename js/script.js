3e103ccff879ce58b07dd57eb46d432ce28299e1
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-03-20 16:36:31 +0200

    екция наши работы, начало секции мастеры

--- js/script.js
+++ js/script.js
@@ -5,26 +5,33 @@ $(document).ready(function() {
     });
     $(document).ready(function(){
         $('.slider').slick({
-            arrows:true,
+            arrows:false,
             dots:true,
             slidesToShow:1,
             autoplay:false,
             speed:1000,
+        });
+        $('.slider_master').slick({
+            arrows:true,
+            dots:true,
+            slidesToShow:3,
+            autoplay:false,
+            speed:1000,
             // autoplaySpeed:800,
-            // responsive:[
-            //     {
-            //         breakpoint: 768,
-            //         settings: {
-            //             slidesToShow:2
-            //         }
-            //     },
-            //     {
-            //         breakpoint: 550,
-            //         settings: {
-            //             slidesToShow:1
-            //         }
-            //     }
-            // ]
+            responsive:[
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow:2
+                    }
+                },
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow:1
+                    }
+                }
+            ]
         });
     });
     
