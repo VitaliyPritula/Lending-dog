8cea4781b994d8747969111d2ad87d651b533659
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-03-22 14:36:24 +0200

    реализовано секцию прайс с кастомными стилями

--- js/script.js
+++ js/script.js
@@ -55,3 +55,128 @@ $("#fullpage").onepage_scroll({
                                      // the browser's width is less than 600, the fallback will kick in.
     direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function () {
+    console.log('test ajax');
+
+    $('#contactform').on('submit', function (e) {
+        e.preventDefault();
+
+        console.log('contactform');
+
+        $.ajax({
+            type: 'POST',
+            url: '/sendmail',
+            data: $('#contactform').serialize(),
+            success: function (data) {
+                if (data.result == 'success') {
+                    console.log('data.result = ', data.result);
+                    $('#senderror').hide();
+                    $('#sendmessage').show();
+                    $('#contactform')[0].reset();
+                }
+
+                if (data.result == 'failed') {
+                    console.log('data = ', data.name);
+                    $('#senderror').show();
+                    $('#sendmessage').hide();
+                }
+            },
+            error: function () {
+                console.log('error ajax');
+                // $('#senderror').show();
+                // $('#sendmessage').hide();
+            }
+        });
+    });
+});
+
+var $page = $('html, body');
+
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 675);
+        return false;
+    });
+
+    /* 2 */
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+            btn.addClass('show');
+        } else {
+            btn.removeClass('show');
+        }
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+    function hideNavbarCollapse() {
+        let navbars_main = $('#navbars-main');
+
+        if (navbars_main.hasClass("show")) {
+            navbars_main.removeClass('show');
+        }
+    }
+    $('.form_datetime').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+    });
+    $('.form_date').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+    $('.form_time').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0
+    });
+
+// $function(m,e,t,r,i,k,a) {m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+//         m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}
+//     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+//     ym(70137172, "init", {
+//         clickmap:true,
+//         trackLinks:true,
+//         accurateTrackBounce:true,
+//         webvisor:true
+//     });
