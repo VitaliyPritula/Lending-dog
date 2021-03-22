f5fe3604ac38a0c6354a6c60ffa22ffeab21c4a1
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-03-22 10:12:59 +0200

    подключены слайды

--- js/script.js
+++ js/script.js
@@ -11,29 +11,47 @@ $(document).ready(function() {
             autoplay:false,
             speed:1000,
         });
-        $('.slider_master').slick({
-            arrows:true,
-            dots:true,
-            slidesToShow:3,
-            autoplay:false,
+        $('.slider-test').not('.slick-initialized').slick({
+            slidesToShow: 1,
+            // dots: true,
+            centerMode: true,
+            centerPagging: '200px',
+            // focusOnSelect: true,
             speed:1000,
+            variableWidth: true
             // autoplaySpeed:800,
-            responsive:[
-                {
-                    breakpoint: 768,
-                    settings: {
-                        slidesToShow:2
-                    }
-                },
-                {
-                    breakpoint: 550,
-                    settings: {
-                        slidesToShow:1
-                    }
-                }
-            ]
+            // responsive:[
+            //     {
+            //         breakpoint: 768,
+            //         settings: {
+            //             slidesToShow:2
+            //         }
+            //     },
+            //     {
+            //         breakpoint: 550,
+            //         settings: {
+            //             slidesToShow:1
+            //         }
+            //     }
+            // ]
         });
     });
     
     
 });
+
+$("#fullpage").onepage_scroll({
+    sectionContainer: "section",     // sectionContainer accepts any kind of selector in case you don't want to use section
+    easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
+                                     // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
+    animationTime: 1000,             // AnimationTime let you define how long each section takes to animate
+    pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+    updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+
+    loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
+    keyboard: true,                  // You can activate the keyboard controls
+    responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
+                                     // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
+                                     // the browser's width is less than 600, the fallback will kick in.
+    direction: "vertical"            // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
+ });
