862e227a5e35afb4e0eea2a6a59f6285e690f6a8
Oleksandr <aleksgmbox@gmail.com>
2021-03-26 12:07:41 +0200

    fixed dots selection after page refreshing

--- js/vertical-dot-navigation.js
+++ js/vertical-dot-navigation.js
@@ -2,7 +2,7 @@ $(document).ready(function ($) {
     var parPosition = [];
     $('section').each(function () {
         parPosition.push($(this).offset().top);
-    });    
+    });
 
     $('nav.dot-vrt-nav a').click(function () {
         $('html, body').animate({
@@ -16,23 +16,27 @@ $(document).ready(function ($) {
         $(this).addClass('active');
     });
 
-    $(document).scroll(function () {
-        var position = $(document).scrollTop(),
-            index;
-        for (var i = parPosition.length - 1; i >= 0; i--) {
-            
-            if (position >= parPosition[i]) {
-                index = i;
-                break;
-            }
-        }        
-        
-        $('nav.dot-vrt-nav  li').removeClass('active');
-        $('nav.dot-vrt-nav  li:eq(' + index + ')').addClass('active');
-    });
+    $(document).scroll(function () { activateDot(); });
 
     $('nav.dot-vrt-nav  li').click(function () {
         $('nav.dot-vrt-nav  li').removeClass('active');
         $(this).addClass('active');
     });
+
+    activateDot();
+
+    function activateDot() {
+        var position = $(document).scrollTop(),
+            index;
+        for (var i = parPosition.length - 1; i >= 0; i--) {
+
+            if (position >= parPosition[i]) {
+                index = i;
+                break;
+            }
+        }
+        
+        $('nav.dot-vrt-nav  li').removeClass('active');
+        $('nav.dot-vrt-nav  li:eq(' + index + ')').addClass('active');
+    };
 });
