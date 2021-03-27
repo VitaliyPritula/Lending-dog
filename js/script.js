479a5bb830e22528a6e6bb7d9e020d3adae729da
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-03-27 10:39:39 +0200

    добаввил форму. подключил бутстрап

--- js/script.js
+++ js/script.js
@@ -3,6 +3,37 @@ $(document).ready(function() {
         $('.header-burger, .menu-nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $('.form_datetime').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1
+    });
+    $('.form_date').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+    $('.form_time').datetimepicker({
+        language:  'ru',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0
+    });
     $(document).ready(function(){
         $('.slider').slick({
             arrows:false,
