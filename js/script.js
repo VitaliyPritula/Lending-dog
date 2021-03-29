3f2c9b9ac357f602f209d285614ab718dcfb8720
Oleksandr <aleksgmbox@gmail.com>
2021-03-29 12:06:25 +0300

    added datetime picker

--- js/script.js
+++ js/script.js
@@ -1,61 +1,50 @@
-$(document).ready(function() {
-    $('.header-burger').click(function(event) {
+$(document).ready(function () {
+    $('.header-burger').click(function (event) {
         $('.header-burger, .menu-nav').toggleClass('active');
         $('body').toggleClass('lock');
     });
-    $('.form_datetime').datetimepicker({
-        language:  'ru',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        forceParse: 0,
-        showMeridian: 1
-    });
-    $('.form_date').datetimepicker({
-        language:  'ru',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 2,
-        minView: 2,
-        forceParse: 0
-    });
-    $('.form_time').datetimepicker({
-        language:  'ru',
-        weekStart: 1,
-        todayBtn:  1,
-        autoclose: 1,
-        todayHighlight: 1,
-        startView: 1,
-        minView: 0,
-        maxView: 1,
-        forceParse: 0
-    });
-    $(document).ready(function(){
+
+    $(document).ready(function () {
         $('.slider').slick({
-            arrows:false,
-            dots:true,
-            slidesToShow:1,
-            autoplay:false,
-            speed:1000,
-        });        
-    });    
+            arrows: false,
+            dots: true,
+            slidesToShow: 1,
+            autoplay: false,
+            speed: 1000,
+        });
+    });
+
+    $(document).ready(function () {
+        $.datetimepicker.setLocale('ru');
+
+        $('#datepicker1').datetimepicker({
+            timepicker: false,
+            format: 'd M y',
+            minDate:'0'
+        });
+        $('#datepicker2').datetimepicker({
+            datepicker: false,
+            format: 'H:i'
+        });
+        $('#datepicker3').datetimepicker({
+            timepicker: false,
+            format: 'd M y',
+            minDate:'0'
+        });
+        $('#datepicker4').datetimepicker({
+            datepicker: false,
+            format: 'H:i'
+        });
+    });
 });
 
-$(function () {
-    $('#datetimepicker2').datetimepicker({
-        locale: 'ru'
-    });
-});
+
 const raitengItemsList = document.querySelectorAll('.raiteng_item');
 const raitengItemsArray = Array.prototype.slice.call(raitengItemsList);
 
 raitengItemsArray.forEach(item =>
-  item.addEventListener('click', () =>{
-    const {itemValue} = item.dataset;
-    item.parentNode.dataset.totalValue = itemValue;
-  })  
-); 
+    item.addEventListener('click', () => {
+        const { itemValue } = item.dataset;
+        item.parentNode.dataset.totalValue = itemValue;
+    })
+);
