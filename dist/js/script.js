1439c2756d5d0bac0d594951dab61f13de8aeed2
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-03-31 17:22:06 +0300

    якорьные ссылки, поднял гамбургер меню.

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.header-burger').click(function (event) {
         $('.header-burger, .menu-nav').toggleClass('active');
         $('body').toggleClass('lock');
+        
     });
 
     $(document).ready(function () {
@@ -37,6 +38,36 @@ $(document).ready(function () {
         });
     });
 });
+
+// плавная прокрутка при скроле
+const menuLinks = document.querySelectorAll('.header_li[data-goto]');
+if(menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick);
+    });
+
+    if(header-burger .menu-nav.contains('active')) {
+        document.body.classList.remove('lock');
+        menu-burger.classList.remove('active');
+        menuBody.classList.remove('active');
+    }
+
+    function onMenuLinkClick(e) {
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+            
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
 
 
 const raitengItemsList = document.querySelectorAll('.raiteng_item');
