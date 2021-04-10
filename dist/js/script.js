29b86dbada8fdcf6dcc206daa899d53b8d9675df
VitaliyPritula <pritula.vitalyiv94@gmail.com>
2021-04-10 09:36:33 +0300

    реализована плавная прокрутка по якорям

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,38 +1,8 @@
 $(document).ready(function () {
     $('.header-burger').click(function (event) {
         $('.header-burger, .menu-nav').toggleClass('active');
-        $('body').toggleClass('lock');        
+        $('body').toggleClass('lock');
     });
-
-// плавная прокрутка при скроле
-const menuLinks = document.querySelectorAll('.header_li[data-goto]');
-if(menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });    
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        var headerBurger = document.querySelector(".header-burger");
-        var menuNav = document.querySelector(".menu-nav");
-        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-
-            if(headerBurger.classList.contains('active')){
-                document.body.classList.remove('lock');
-                headerBurger.classList.remove('active');
-                menuNav.classList.remove('active');
-            }
-            
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
-        }
-    }
-}
-
 });
 
 $(document).ready(function () {
@@ -42,23 +12,20 @@ $(document).ready(function () {
         slidesToShow: 1,
         autoplay: false,
         speed: 1000,
-        // responsive:[
-		// 	{
-		// 		breakpoint: 768,
-		// 		settings: {
-		// 			slidesToShow:2
-		// 		}
-		// 	},
-		// 	{
-		// 		breakpoint: 425,
-		// 		settings: {
-		// 			slidesToShow:1,
-        //             slidesPerRow:1
-		// 		}
-		// 	}
-		// ]
     });
 });
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+
+});
+
+
 
 const raitengItemsList = document.querySelectorAll('.raiteng_item');
 const raitengItemsArray = Array.prototype.slice.call(raitengItemsList);
